99821eb124dcc85aefc4a19baa3ba6f145260e66
leejisung <32594290+lee-jisung@users.noreply.github.com>
2019-02-13 06:22:37 +0900

    Create 11004.cpp

---
+++ baekjoon/11004.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int a[5000001];
+
+int main(void) {
+
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	sort(a, a + n);
+	printf("%d", a[k - 1]);
+
+
+	return 0;
+}
